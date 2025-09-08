41b3f12c2b147dc01f53d65199242b35cd47b331
Roqya-Yawari <roqiayawari33@gmail.com>
2025-09-08 18:10:58 +0430

    change width of accordion

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import star from "../src/assets/images/icon-star.svg";
 function App() {
   return (
     <>
-      <div className="container absolute   left-1/2  -translate-x-1/2 rounded-md  bg-white w-100  shadow-sm shadow-gray-200 p-3  top-[10%] md:top-1/2 md:-translate-y-1/2 max-sm:w-[250px] md:w-[400px]  ">
+      <div className="container absolute   left-1/2  -translate-x-1/2 rounded-md  bg-white w-100  shadow-sm shadow-gray-200 p-3  top-[10%] md:top-1/2 md:-translate-y-1/2 max-sm:w-[300px] md:w-[400px]  ">
         <div className="flex justify-start p-3 items-center gap-3">
           <img className="w-5" src={star} alt="star icon" />
           <h1 className="text-2xl font-bold text-purple-950">FAQs</h1>
