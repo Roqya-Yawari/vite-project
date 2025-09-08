7163662e803ab0f1bac9a4ea5a89f4917651b59f
Roqya-Yawari <roqiayawari33@gmail.com>
2025-09-08 18:03:10 +0430

    add star icon

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/vite-project/",
+  base: "vite-project",
 
   plugins: [react(), tailwindcss()],
 });
