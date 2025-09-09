ad2c9269c5c1f380b023344a8a83a7635b0a07ca
Roqya-Yawari <roqiayawari33@gmail.com>
2025-09-09 11:23:54 +0430

    perf enhance the accordion sothat when one is open , next one should be closed

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,12 @@
 import "./App.css";
 import Accordian from "./components/Accordian";
 import star from "../src/assets/images/icon-star.svg";
+import { useState } from "react";
+import { accordions } from "./data";
 
 function App() {
+  const [open, setOpen] = useState(0);
+
   return (
     <>
       <div className="container absolute   left-1/2  -translate-x-1/2 rounded-md  bg-white w-100  shadow-sm shadow-gray-200 p-3  top-[10%] md:top-1/2 md:-translate-y-1/2 max-sm:w-[300px] md:w-[400px]  ">
@@ -12,73 +16,18 @@ function App() {
           <img className="w-5" src={star} alt="star icon" />
           <h1 className="text-2xl font-bold text-purple-950">FAQs</h1>
         </div>
-        <Accordian
-          defaultAccordion={false}
-          title="What is Frontend Mentor, and how will it help me?"
-          describtion="Frontend Mentor offers realistic coding challenges to help developers improve their frontend coding skills with projects in HTML, CSS, and JavaScript. It's suitable for all levels and ideal for portfolio building."
-        />
-        <Accordian
-          defaultAccordion={true}
-          title="Can I use Frontend Mentor projects in my portfolio?"
-          describtion="Frontend Mentor offers realistic coding challenges to help developers improve their frontend coding skills with projects in HTML, CSS, and JavaScript. It's suitable for all levels and ideal for portfolio building."
-        />{" "}
-        <Accordian
-          defaultAccordion={true}
-          title="How can I get help if I'm stuch on a challenge?"
-          describtion="Frontend Mentor offers realistic coding challenges to help developers improve their frontend coding skills with projects in HTML, CSS, and JavaScript. It's suitable for all levels and ideal for portfolio building."
-        />{" "}
-        <Accordian
-          defaultAccordion={true}
-          title="Is Frontend Mentor free?"
-          describtion="Frontend Mentor offers realistic coding challenges to help developers improve their frontend coding skills with projects in HTML, CSS, and JavaScript. It's suitable for all levels and ideal for portfolio building."
-        />
+        {accordions.map((accordion, index) => (
+          <Accordian
+            key={index}
+            title={accordion.title}
+            describtion={accordion.describtion}
+            open={open === index}
+            onClick={() => setOpen(open === index ? null : index)}
+          />
+        ))}
       </div>
     </>
   );
 }
 
 export default App;
-// -----------------------------------------------------------
-// import React, { useState } from "react";
-// import plusicon from "../src/assets/images/icon-plus.svg";
-// import minusicon from "../src/assets/images/icon-minus.svg";
-
-// // Plus icon component
-// function PlusIcon({ onClick }) {
-//   return (
-//     <img
-//       src={plusicon}
-//       alt="plus"
-//       className="w-5 cursor-pointer"
-//       onClick={onClick}
-//     />
-//   );
-// }
-// // Minus icon component
-// function MinusIcon({ onClick }) {
-//   return (
-//     <img
-//       src={minusicon}
-//       alt="minus"
-//       className="w-5 cursor-pointer"
-//       onClick={onClick}
-//     />
-//   );
-// }
-
-// // Accordion component
-// export default function Accordion() {
-//   const [open, setOpen] = useState(false);
-
-//   return (
-//     <div className="p-4 border rounded w-64">
-//       {open ? (
-//         <MinusIcon onClick={() => setOpen(false)} />
-//       ) : (
-//         <PlusIcon onClick={() => setOpen(true)} />
-//       )}
-
-//       {open && <p className="mt-2 text-xs text-purple-800">hhhhhhhh</p>}
-//     </div>
-//   );
-// }
